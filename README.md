9a9a0d2a1d493b82b0c6dbee91f7b09a3290e9f3
selectpdf <support@selectpdf.com>
2015-11-12 11:48:40 +0200

    Java Samples for SelectPdf HTML to PDF REST API

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # selectpdf-api-java-samples
+
+This repository contains Java samples for the SelectPdf [Html To Pdf REST API](http://selectpdf.com/html-to-pdf-api/).
+
+SelectPdf HTML To PDF Online REST API is a professional solution that lets you create PDF from web pages and raw HTML code in your applications. 
+The API is easy to use and the integration takes only a few lines of code.
+
+SelectPdf [Html To Pdf](http://selectpdf.com) API can be called from any platform: Java, PHP, .NET, Ruby, Perl, etc.
---
+++ TestSelectPdf1.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+/*
+ * This code converts an url to pdf in Java using SelectPdf REST API through a GET request. The content is saved into a file on the disk.
+ *
+ */
+public class TestSelectPdf1 {
+    public static void main(String[] args) throws Exception {
+		HttpURLConnection urlConnection = null;
+
+		try {
+			String apiEndpoint = "http://selectpdf.com/api2/convert/";
+			String key = "your license key here";
+			String testUrl = "http://selectpdf.com";
+			File localFile = new File("test.pdf");
+
+			Map<String,Object> parameters = new LinkedHashMap<>();
+			parameters.put("key", key);
+			parameters.put("url", testUrl);
+
+			String encodedParameters = encodeParameters(parameters);
+
+			URL apiUrl = new URL(apiEndpoint + "?" + encodedParameters);
+			urlConnection = (HttpURLConnection)apiUrl.openConnection();
+			BufferedInputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
+			BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(localFile));
+
+			byte[] b = new byte[8 * 1024];
+			int read = 0;
+			while ((read = inputStream.read(b)) > -1) {
+				outputStream.write(b, 0, read);
+			}
+			outputStream.flush();
+			outputStream.close();
+			inputStream.close();
+
+			System.out.println("Test pdf document generated successfully!"); 
+		}
+		catch (Exception ex) {
+			System.out.println("Error: " + ex.getMessage());
+
+			if (urlConnection != null) {
+				if (urlConnection.getResponseCode() != 200) {
+					System.out.println("HTTP Response Code: " + urlConnection.getResponseCode());
+					System.out.println("HTTP Response Message: " + urlConnection.getResponseMessage());
+				}
+			}
+		}
+    }
+
+	private static String encodeParameters(Map<String,Object> params) throws Exception {
+		try {
+			StringBuilder data = new StringBuilder();
+			for (Map.Entry<String,Object> param : params.entrySet()) {
+				if (data.length() != 0) data.append('&');
+				data.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+				data.append('=');
+				data.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+			}
+
+			return data.toString();
+		}
+		catch (Exception ex) {
+			throw(ex);
+		}
+	}
+}
